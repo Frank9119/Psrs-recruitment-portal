dc10a66d0c421b07c0b54a751c2cc021f685c33e
frank.alex <frank.alex@eganet.go.tz>
2026-01-28 17:24:47 +0300

    Fix gitignore and remove ignored files from repo

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 # Psrs-recruitment-portal
 Full-stack recruitment system
+
+
+## Project Structure
+1. `psrs_accounts`:  
+    User managements, Separation and profile managements. Hapa utaasign users kulingana na nafasi zao. Mfano: Admin, Management na category Admin.
+
+1. `psrs_auth`: 
+    User managements, Separation and profile managements. Hapa utaasign users kulingana na roles zao. Mfano: Admin, Applicants na Recuiter. 
+
+1. `psrs_job`:   
+    Manage all Jobs Applications. 
+
+1. `psrs_settings`:  
+    Settings zote za mfumo zinafanyika hapa. kitu chochote ambacho ni confugurable kinafanika hapa. Mfano: Application types.
+
+
+## Running the Application
+
+1. **Local host** `python manage.py runserver 0.0.0.0:8000`
+
+### Database configurations
+Mysql database is used.
--- psrs_auth/admin.py
+++ psrs_auth/admin.py
@@ -1,3 +1,4 @@
+# from rest_framework.authtoken.admin import TokenAdmin
 from django.contrib import admin
 from psrs_auth.models import User, UserProfile
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
@@ -15,8 +16,70 @@ class UserAdmin(BaseUserAdmin):
     list_display = ["username", "first_name", "last_name", "email", "role"]
     list_filter = ["role", "first_name", "last_name"]
 
+    add_fieldsets = (
+        (
+            None,
+            {
+                "classes": ("wide",),
+                "fields": (
+                    "username",
+                    "first_name", 
+                    "last_name", 
+                    "email",
+                    "role",
+                    "password1",
+                    "password2",
+
+                ),
+
+            }
+        ),
+    )
+    fieldsets = (
+        (
+            None,
+            {
+                "fields": (
+                    "username",
+                    "first_name", 
+                    "last_name", 
+                    "email",
+                    "role",
+                    "password1",
+                    "password2",
+
+                ),
+            }
+        ),
+    )
+    ordering = ["email"]
+
+
+
+@admin.register(UserProfile)
+class UserProfileAdmin(admin.ModelAdmin):
+    search_fields = ["user__first_name", "user__last_name", "user__email", "user__username"]
+    list_display = ["first_name", "last_name", "phone_number", "points"]
+    list_filter = ["user__first_name", "user__last_name"]
+
+    def get_queryset(self, request):
+        qs = super().get_queryset(request)
+        return qs.select_related("user")
+
+    def user(self, obj):
+        return obj.user.username if obj.user else None
+    
+    user.admin_order_field = "user__username"
+    user.short_description = "User"
+    user.empty_value_display = "N/A"
+    user.admin_order_field = "user__username"
+    user.short_description = "User"
+    user.empty_value_display = "N/A"
+    user.admin_order_field = "user__username"
+
+
+# TokenAdmin.raw_id_fields = ["suer"]
+
     
 
-
-    ordering = ["email"]
 
